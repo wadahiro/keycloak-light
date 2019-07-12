6af0a9bd8046b8573980c88ba91397c9dc88cbf7
Hiroyuki Wada <wadahiro@gmail.com>
2019-07-12 23:22:26 +0900

    Add build for cl4cds

--- build.sh
+++ build.sh
@@ -5,9 +5,12 @@ docker build -f Dockerfile-openjdk12 -t wadahiro/openjdk12 .
 docker build -f Dockerfile-openjdk13ea -t wadahiro/openjdk13ea .
 docker build -f Dockerfile-openjdk12-openj9 -t wadahiro/openjdk12-openj9 .
 
+docker build -f Dockerfile-cl4cds -t wadahiro/cl4cds .
+
 # Keycloak images
 docker build -f Dockerfile-openjdk12-openj9-keycloak -t wadahiro/keycloak-light-openjdk12-openj9 .
 docker build -f Dockerfile-openjdk12-keycloak -t wadahiro/keycloak-light-openjdk12 .
+docker build -f Dockerfile-openjdk12-keycloak2 -t wadahiro/keycloak-light-openjdk12-cl4cds .
 docker build -f Dockerfile-openjdk13ea-keycloak -t wadahiro/keycloak-light-openjdk13ea .
 
 
