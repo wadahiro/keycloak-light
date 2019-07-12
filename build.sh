03a72ec982b95c0fa2c0940905a749dbc8ace32f
Hiroyuki Wada <wadahiro@gmail.com>
2019-07-12 19:23:47 +0900

    Add keycloak with openjdk13ea

--- build.sh
+++ build.sh
@@ -5,5 +5,6 @@ docker build -f Dockerfile-openjdk12-openj9 -t wadahiro/openjdk12-openj9 .
 
 # Keycloak images
 docker build -f Dockerfile-openjdk12-openj9-keycloak -t wadahiro/keycloak-light-openjdk12-openj9 .
+docker build -f Dockerfile-openjdk13ea-keycloak -t wadahiro/keycloak-light-openjdk13ea .
 
 
