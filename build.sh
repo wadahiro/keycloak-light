3b739b00d5892a11328aa678003eb4dd9e98c7b7
Hiroyuki Wada <wadahiro@gmail.com>
2019-07-18 04:34:36 +0000

    Refactoring

--- build.sh
+++ build.sh
@@ -9,6 +9,7 @@ docker build -f Dockerfile-keycloak -t wadahiro/keycloak .
 docker build -f Dockerfile-keycloak-spring-boot -t wadahiro/keycloak-spring-boot .
 
 # Keycloak images
+docker build -f Dockerfile-keycloak-official -t wadahiro/keycloak-official .
 docker build -f Dockerfile-openjdk11-keycloak -t wadahiro/openjdk11-keycloak .
 docker build -f Dockerfile-openjdk12-keycloak -t wadahiro/openjdk12-keycloak .
 docker build -f Dockerfile-openjdk13ea-keycloak -t wadahiro/openjdk13ea-keycloak .
@@ -20,8 +21,12 @@ docker build -f Dockerfile-openjdk12-keycloak-cds -t wadahiro/openjdk12-keycloak
 docker build -f Dockerfile-openjdk13ea-keycloak-cds -t wadahiro/openjdk13ea-keycloak-cds .
 docker build -f Dockerfile-openjdk12-openj9-keycloak-cds -t wadahiro/openjdk12-openj9-keycloak-cds .
 
-# Spring
-#docker build -f Dockerfile-openjdk12-keycloak-spring-boot-cl4cds -t wadahiro/keycloak-spring-boot-openjdk12-cl4cds .
-#docker build -f Dockerfile-keycloak-spring-boot-openjdk12-openj9 -t wadahiro/keycloak-spring-boot-openjdk12-openj9 .
+# Keycloak with spring boot
+docker build -f Dockerfile-openjdk12-openj9-keycloak-spring-boot -t wadahiro/openjdk12-openj9-keycloak-spring-boot .
+docker build -f Dockerfile-openjdk13ea-keycloak-spring-boot -t wadahiro/openjdk13ea-keycloak-spring-boot .
+
+# Keycloak with spring boot with CDS
+docker build -f Dockerfile-openjdk12-openj9-keycloak-spring-boot-cds -t wadahiro/openjdk12-openj9-keycloak-spring-boot-cds .
+docker build -f Dockerfile-openjdk13ea-keycloak-spring-boot-cds -t wadahiro/openjdk13ea-keycloak-spring-boot-cds .
 
 
